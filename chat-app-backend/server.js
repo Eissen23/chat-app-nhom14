132dbf46590349c6bbbae8c5e4377c420eecde26
Phạm Đức Phúc <87574901+Eissen23@users.noreply.github.com>
2024-12-24 20:26:16 +0700

    ok

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -1,2 +1,20 @@
 const express = require('express')
 const app = express()
+
+const {
+    createUser,
+    getUser,
+    createChatRoom,
+    getChatRoomsForUser,
+    getChatMessages,
+    createChatMessage
+} = require('./database')
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+});
