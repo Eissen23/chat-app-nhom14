b5e4f29951f5cd11cd5f527759c02f2958a0f4f5
Phạm Đức Phúc <87574901+Eissen23@users.noreply.github.com>
2024-12-24 16:56:19 +0700

    okey dokey

--- chat-app-backend/database.js
+++ chat-app-backend/database.js
@@ -1,18 +1,30 @@
 const mysql = require('mysql2');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 mysql.createPool(
     {
-        host: '127.0.0.1',
-        user:'root',
-        password: 'DucPhuc2',
-        database: 'chat_app_server',
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
     }
 ).promise()
 
 
-
-
-
+async function createUser(username, email, password) {
+    const connection = mysql.createPool().promise();
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    const [result] = await connection.execute(query, [username, email, password]);
+    return result.insertId;
+}
+async function getUser(userId) {
+    const connection = mysql.createPool().promise();
+    const query = 'SELECT * FROM users WHERE id = ?';
+    const [rows] = await connection.execute(query, [userId]);
+    return rows[0];
+}
 async function createMessage(roomId, userId, message) {
     const connection = mysql.createPool().promise();
     const query = 'INSERT INTO messages (room_id, user_id, message) VALUES (?, ?, ?)';
