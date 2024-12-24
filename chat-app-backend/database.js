86387d9d93faf23d38f92fcc817d3913e2b2a158
Phạm Đức Phúc <87574901+Eissen23@users.noreply.github.com>
2024-12-24 17:39:00 +0700

    add db

--- chat-app-backend/database.js
+++ chat-app-backend/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mysql.createPool(
+const pool = mysql.createPool(
     {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
@@ -14,27 +14,18 @@ mysql.createPool(
 
 
 async function createUser(username, email, password) {
-    const connection = mysql.createPool().promise();
-    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-    const [result] = await connection.execute(query, [username, email, password]);
-    return result.insertId;
-}
-async function getUser(userId) {
-    const connection = mysql.createPool().promise();
-    const query = 'SELECT * FROM users WHERE id = ?';
-    const [rows] = await connection.execute(query, [userId]);
-    return rows[0];
-}
-async function createMessage(roomId, userId, message) {
-    const connection = mysql.createPool().promise();
-    const query = 'INSERT INTO messages (room_id, user_id, message) VALUES (?, ?, ?)';
-    const [result] = await connection.execute(query, [roomId, userId, message]);
-    return result.insertId;
+    const result = await pool.query(
+        'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+        [username, email, password]
+    )
+    return result;
 }
 
-async function getMessages(roomId) {
-    const connection = mysql.createPool().promise();
-    const query = 'SELECT * FROM messages WHERE room_id = ? ORDER BY created_at ASC';
-    const [rows] = await connection.execute(query, [roomId]);
+async function getUser(user_id) {
+    const [rows] = await pool.query('SELECT * FROM users WHERE user_id = ?', [user_id]);
     return rows;
 }
+
+// createUser('bob2','bob2@gmail.com','password').then(result => console.log(result)).catch(err => console.error(err));
+
+// // getUser(3).then(result => console.log(result)).catch(err => console.error(err));
